a4c81a0b3e186f1e906ee402c8b76d5b67e2191f
Toshiki Takakubo <219tosiki@gmail.com>
2024-07-07 20:48:34 +0900

    feat: add database connect function

---
+++ internal/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func InitDB() *sql.DB {
+	host := "postgres"     //os.Getenv("DB_HOST")
+	user := "user"         //os.Getenv("DB_USER")
+	password := "password" //os.Getenv("DB_PASSWORD")
+	dbname := "db"         //os.Getenv("DB_NAME")
+	sslmode := "disable"   //os.Getenv("DB_SSLMODE")
+
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=%s", host, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
