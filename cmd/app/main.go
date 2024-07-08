97018220b396420f15f1d25ed10c005aec169103
Toshiki Takakubo <219tosiki@gmail.com>
2024-07-08 21:11:23 +0900

    feat: add example for getting data from db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/coworker-match-api/internal/db"
+	"github.com/coworker-match-api/internal/models"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	db := db.InitDB()
+	defer db.Close()
+
+	getUserSQL := `SELECT * FROM users`
+	rows, err := db.Query(getUserSQL)
+	if err != nil {
+		log.Fatalf("Error querying data: %v", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user models.User
+		err := rows.Scan(&user.UserID, &user.UserName, &user.Email, &user.AvatarURL, &user.Age, &user.Gender, &user.Birthplace, &user.JobType, &user.LINEAccount, &user.DiscordAccount, &user.Biography, &user.CreatedAt, &user.UpdatedAt)
+		if err != nil {
+			log.Fatalf("Error scanning data: %v", err)
+		}
+		fmt.Printf("User: %+v\n", user)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatalf("Error with rows: %v", err)
+	}
 }
--- internal/db/db.go
+++ internal/db/db.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func InitDB() *sql.DB {
