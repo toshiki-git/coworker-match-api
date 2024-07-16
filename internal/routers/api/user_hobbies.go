4b43d514fe0b04af63f72f328d857c6356d66a3d
Toshiki Takakubo <219tosiki@gmail.com>
2024-07-16 18:52:33 +0900

    refactor: Update user_hobbies API endpoint to support updating user hobbies

--- internal/routers/api/user_hobbies.go
+++ internal/routers/api/user_hobbies.go
@@ -9,7 +9,6 @@ import (
 )
 
 type CreateUserHobbyRequest struct {
-	UserID   string   `json:"user_id"`
 	HobbyIDs []string `json:"hobby_ids"`
 }
 
@@ -24,20 +23,72 @@ func (h *Handler) UserHobbyHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		handleGetUserHobbies(w, h.DB, userID)
+	case http.MethodPut:
+		handlePutUserHobbies(w, r, h.DB, userID)
 	default:
 		writeError(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
 func (h *Handler) CreateUserHobbyHandler(w http.ResponseWriter, r *http.Request) {
+	userID, ok := GetUserID(r.Context())
+	if !ok {
+		writeError(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
 	if r.Method != http.MethodPost {
 		writeError(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	handlePostUserHobby(w, r, h.DB)
+	handlePostUserHobby(w, r, h.DB, userID)
 }
 
-func handlePostUserHobby(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+func handlePutUserHobbies(w http.ResponseWriter, r *http.Request, db *sql.DB, userID string) {
+	var req CreateUserHobbyRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeError(w, fmt.Sprintf("Error decoding request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	// トランザクションを開始
+	tx, err := db.Begin()
+	if err != nil {
+		http.Error(w, "Error starting transaction", http.StatusInternalServerError)
+		return
+	}
+	defer tx.Rollback()
+
+	// user_hobbies テーブルから古いエントリを削除
+	_, err = tx.Exec(`DELETE FROM user_hobbies WHERE user_id = $1`, userID)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error deleting data: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// user_hobbies テーブルに新しいエントリを挿入
+	for _, hobbyID := range req.HobbyIDs {
+		_, err := tx.Exec(`
+			INSERT INTO user_hobbies (user_id, hobby_id)
+			VALUES ($1, $2)
+		`, userID, hobbyID)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error inserting data: %v", err), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// トランザクションをコミット
+	if err := tx.Commit(); err != nil {
+		http.Error(w, "Error committing transaction", http.StatusInternalServerError)
+		return
+	}
+
+	respondWithJSON(w, req)
+
+}
+
+func handlePostUserHobby(w http.ResponseWriter, r *http.Request, db *sql.DB, userID string) {
 	var req CreateUserHobbyRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
@@ -58,7 +109,7 @@ func handlePostUserHobby(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		_, err := tx.Exec(`
 			INSERT INTO user_hobbies (user_id, hobby_id)
 			VALUES ($1, $2)
-		`, req.UserID, hobbyID)
+		`, userID, hobbyID)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Error inserting data: %v", err), http.StatusInternalServerError)
 			return
