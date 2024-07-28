bcf7d80456e8a7b15d29bea07ef0ff320f40732f
Toshiki Takakubo <219tosiki@gmail.com>
2024-07-28 21:14:48 +0900

    refactor: Update GetQuestionCardResponseInner model and API endpoint to include IsUsed field

--- internal/routers/api/messages.go
+++ internal/routers/api/messages.go
@@ -6,51 +6,9 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
-	"time"
+
+	models "github.com/coworker-match-api/gen/go"
 )
-
-type CreateMessageHandlerRequest struct {
-	MatchingID     string `json:"matching_id"`
-	QuestionCardID string `json:"question_card_id"`
-}
-
-type MessagePair struct {
-	Me struct {
-		MessageID   string    `json:"message_id"`
-		MessageText string    `json:"message_text"`
-		CreatedAt   time.Time `json:"created_at"`
-		UpdatedAt   time.Time `json:"updated_at"`
-	} `json:"me"`
-	You struct {
-		MessageID   string    `json:"message_id"`
-		MessageText string    `json:"message_text"`
-		CreatedAt   time.Time `json:"created_at"`
-		UpdatedAt   time.Time `json:"updated_at"`
-	} `json:"you"`
-}
-
-type Message struct {
-	QuestionID   string      `json:"question_id"`
-	QuestionText string      `json:"question_text"`
-	CreatedAt    time.Time   `json:"created_at"`
-	MessagePair  MessagePair `json:"message_pair"`
-}
-
-type MatchUser struct {
-	UserID    string   `json:"user_id"`
-	UserName  string   `json:"user_name"`
-	AvatarURL string   `json:"avatar_url"`
-	Hobbies   []string `json:"hobbies"`
-}
-
-type MainData struct {
-	MatchUser MatchUser `json:"match_user"`
-	Messages  []Message `json:"messages"`
-}
-
-type UpdateMessageHandlerRequest struct {
-	Answer string `json:"answer"`
-}
 
 func (h *Handler) MessageHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -82,41 +40,42 @@ func handleGetMessages(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	// 相手のユーザー情報とメッセージを取得するクエリ
 	query := `
 		SELECT 
-			other_user.user_id, 
-			other_user.user_name, 
-			other_user.avatar_url, 
-			array_agg(h.hobby_name) AS hobbies, 
-			msg.message_id, 
-			msg.message_text,
-			msg.created_at, 
-			msg.updated_at, 
-			q.question_card_id AS question_id, 
-			q.question_card_text AS question_text, 
-			q.created_at AS question_created_at,
-			msg.user_id
+			qc.question_card_id,
+			qc.question_card_text,
+			me_msg.message_id me_message_id,
+			me_msg.message_text me_message_text,
+			you_msg.message_id you_message_id,
+			you_msg.message_text you_message_text
 		FROM 
-			matchings AS m
-		LEFT JOIN 
-			users AS other_user ON 
-				(m.sender_user_id = other_user.user_id AND m.receiver_user_id = $1)
-				OR 
-				(m.receiver_user_id = other_user.user_id AND m.sender_user_id = $1)
-		LEFT JOIN 
-			messages AS msg ON m.matching_id = msg.matching_id
-		LEFT JOIN 
-			question_cards AS q ON msg.question_card_id = q.question_card_id
-		LEFT JOIN 
-			user_hobbies uh ON uh.user_id = other_user.user_id
-		LEFT JOIN 
-			hobbies h ON h.hobby_id = uh.hobby_id
+			matchings m
+		JOIN (
+			SELECT
+				message_text,
+				matching_id,
+				question_card_id,
+				message_id
+			FROM
+				messages
+			WHERE
+				user_id = $1
+			) me_msg ON m.matching_id = me_msg.matching_id
+		JOIN (
+			SELECT
+				message_text,
+				matching_id,
+				question_card_id,
+				message_id
+			FROM
+				messages you_msg
+			WHERE
+				user_id != $1
+			) you_msg ON m.matching_id = you_msg.matching_id
+		JOIN question_cards qc ON me_msg.question_card_id = qc.question_card_id AND you_msg.question_card_id = qc.question_card_id
 		WHERE 
 			m.matching_id = $2
-		GROUP BY 
-			other_user.user_id, msg.message_id, q.question_card_id, msg.user_id
-		ORDER BY
-			msg.created_at ASC
 	`
 
+	var response models.GetMessageResponse
 	rows, err := db.Query(query, userID, matchingID)
 	if err != nil {
 		writeError(w, fmt.Sprintf("Error querying messages: %v", err), http.StatusInternalServerError)
@@ -124,53 +83,16 @@ func handleGetMessages(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	}
 	defer rows.Close()
 
-	var mainData MainData
-	messageMap := make(map[string]*Message) // question_idをキーにする
-
 	for rows.Next() {
-		var matchUser MatchUser
-		var hobbies []byte
-		var questionID, questionText, messageUserID, messageID, messageText sql.NullString
-		var createdAt, updatedAt, questionCreatedAt sql.NullTime
-
-		if err := rows.Scan(
-			&matchUser.UserID, &matchUser.UserName, &matchUser.AvatarURL, &hobbies,
-			&messageID, &messageText, &createdAt, &updatedAt,
-			&questionID, &questionText, &questionCreatedAt, &messageUserID); err != nil {
+		var data models.GetMessageResponseMessagesInner
+		if err := rows.Scan(&data.QuestionCardId, &data.QuestionCardText, &data.MessagePair.Me.MessageId, &data.MessagePair.Me.MessageText, &data.MessagePair.You.MessageId, &data.MessagePair.You.MessageText); err != nil {
 			writeError(w, fmt.Sprintf("Error scanning row: %v", err), http.StatusInternalServerError)
 			return
 		}
-
-		matchUser.Hobbies = []string{}
-		json.Unmarshal(hobbies, &matchUser.Hobbies)
-		mainData.MatchUser = matchUser
-
-		if _, exists := messageMap[questionID.String]; !exists {
-			messageMap[questionID.String] = &Message{
-				QuestionID:   questionID.String,
-				QuestionText: questionText.String,
-				CreatedAt:    questionCreatedAt.Time,
-			}
-		}
-
-		if messageUserID.String == userID {
-			messageMap[questionID.String].MessagePair.Me.MessageID = messageID.String
-			messageMap[questionID.String].MessagePair.Me.MessageText = messageText.String
-			messageMap[questionID.String].MessagePair.Me.CreatedAt = createdAt.Time
-			messageMap[questionID.String].MessagePair.Me.UpdatedAt = updatedAt.Time
-		} else {
-			messageMap[questionID.String].MessagePair.You.MessageID = messageID.String
-			messageMap[questionID.String].MessagePair.You.MessageText = messageText.String
-			messageMap[questionID.String].MessagePair.You.CreatedAt = createdAt.Time
-			messageMap[questionID.String].MessagePair.You.UpdatedAt = updatedAt.Time
-		}
+		response.Messages = append(response.Messages, data)
 	}
 
-	for _, msg := range messageMap {
-		mainData.Messages = append(mainData.Messages, *msg)
-	}
-
-	respondWithJSON(w, mainData)
+	respondWithJSON(w, response)
 
 	// メッセージをis_readに更新するクエリ
 	updateQuery := `
@@ -188,7 +110,9 @@ func handleGetMessages(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 }
 
 func handlePostMessage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
-	var req CreateMessageHandlerRequest
+	var req models.CreateMessageRequest
+
+	matchingID := r.URL.Query().Get("matching_id")
 
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		writeError(w, fmt.Sprintf("Error decoding request body: %v", err), http.StatusBadRequest)
@@ -214,7 +138,7 @@ func handlePostMessage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 		WHERE 
 			matching_id = $2`
 
-	if err := db.QueryRow(query, userID, req.MatchingID).Scan(&otherUserID); err != nil {
+	if err := db.QueryRow(query, userID, matchingID).Scan(&otherUserID); err != nil {
 		writeError(w, fmt.Sprintf("Error querying other user ID: %v", err), http.StatusInternalServerError)
 		return
 	}
@@ -239,7 +163,7 @@ func handlePostMessage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	err = tx.QueryRow(`
 		INSERT INTO messages (matching_id, question_card_id, user_id)
 		VALUES ($1, $2, $3)
-		RETURNING message_id`, req.MatchingID, req.QuestionCardID, userID).Scan(&messageID)
+		RETURNING message_id`, matchingID, req.QuestionCardId, userID).Scan(&messageID)
 	if err != nil {
 		writeError(w, fmt.Sprintf("Failed to insert message for user: %v", err), http.StatusInternalServerError)
 		return
@@ -248,7 +172,7 @@ func handlePostMessage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	// 相手のIDでメッセージを作成
 	_, err = tx.Exec(`
 		INSERT INTO messages (matching_id, question_card_id, user_id)
-		VALUES ($1, $2, $3)`, req.MatchingID, req.QuestionCardID, otherUserID)
+		VALUES ($1, $2, $3)`, matchingID, req.QuestionCardId, otherUserID)
 	if err != nil {
 		writeError(w, fmt.Sprintf("Failed to insert message for other user: %v", err), http.StatusInternalServerError)
 		return
@@ -262,6 +186,9 @@ func handlePostMessage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	respondWithJSON(w, response)
 }
 func handlePutMessage(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	type UpdateMessageHandlerRequest struct {
+		Answer string `json:"answer"`
+	}
 	var req UpdateMessageHandlerRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		writeError(w, fmt.Sprintf("Error decoding request body: %v", err), http.StatusBadRequest)
