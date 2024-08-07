1fa33b3272c41943979c4fb7e2b0ee8d5a487225
Toshiki Takakubo <219tosiki@gmail.com>
2024-08-07 21:11:55 +0900

    refactor: Add endpoints for matching questions, question cards, matchings, and messages

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,28 +6,46 @@ import (
 
 	"github.com/coworker-match-api/internal/controllers"
 	"github.com/coworker-match-api/internal/repositories"
-	"github.com/coworker-match-api/internal/routers/api"
 	"github.com/coworker-match-api/internal/routers/middleware"
 	"github.com/coworker-match-api/internal/usecases"
 	"github.com/gorilla/mux"
 )
 
 func InitRouter(db *sql.DB) http.Handler {
-	h := api.NewHandler(db)
 	userRepo := repositories.NewUserRepo(db)
 	userUsecase := usecases.NewUserUsecase(userRepo)
 	userController := controllers.NewUserController(userUsecase)
+
 	hobbyRepo := repositories.NewHobbyRepo(db)
 	hobbyUsecase := usecases.NewHobbyUsecase(hobbyRepo)
 	hobbyController := controllers.NewHobbyController(hobbyUsecase)
+
 	userHobbyRepo := repositories.NewUserHobbyRepo(db)
 	userHobbyUsecase := usecases.NewUserHobbyUsecase(userHobbyRepo)
 	userHobbyController := controllers.NewUserHobbyController(userHobbyUsecase)
 
+	questionCardRepo := repositories.NewQuestionCardRepo(db)
+	questionCardUsecase := usecases.NewQuestionCardUsecase(questionCardRepo)
+	questionCardController := controllers.NewQuestionCardController(questionCardUsecase)
+
+	matchingQuestionRepo := repositories.NewMatchingQuestionRepo(db)
+	matchingQuestionUsecase := usecases.NewMatchingQuestionUsecase(matchingQuestionRepo)
+	matchingQuestionController := controllers.NewMatchingQuestionController(matchingQuestionUsecase)
+
+	matchingRepo := repositories.NewMatchingRepo(db)
+	matchingUsecase := usecases.NewMatchingUsecase(matchingRepo)
+	matchingController := controllers.NewMatchingController(matchingUsecase)
+
+	messageRepo := repositories.NewMessageRepo(db)
+	messageUsecase := usecases.NewMessageUsecase(messageRepo)
+	messageController := controllers.NewMessageController(messageUsecase)
+
 	router := mux.NewRouter()
 
 	// 非認証エンドポイント
-	router.HandleFunc("/api/ping", h.PingHandler).Methods("GET")
+	router.HandleFunc("/api/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	}).Methods("GET")
 
 	// 認証エンドポイントのグループ化
 	authRouter := router.PathPrefix("/api").Subrouter()
@@ -40,16 +58,21 @@ func InitRouter(db *sql.DB) http.Handler {
 
 	authRouter.HandleFunc("/hobbies", hobbyController.GetAllHobby).Methods("GET")
 
-	authRouter.HandleFunc("/user_hobbies", userHobbyController.CreateUserHobby).Methods("POST")
-	authRouter.HandleFunc("/user_hobbies", userHobbyController.UpdateUserHobby).Methods("PUT")
-	authRouter.HandleFunc("/user_hobbies/{userId}", userHobbyController.GetAllUserHobby).Methods("GET")
+	authRouter.HandleFunc("/user-hobbies", userHobbyController.CreateUserHobby).Methods("POST")
+	authRouter.HandleFunc("/user-hobbies", userHobbyController.UpdateUserHobby).Methods("PUT")
+	authRouter.HandleFunc("/user-hobbies/{userId}", userHobbyController.GetAllUserHobby).Methods("GET")
 
-	authRouter.HandleFunc("/matching_questions", h.MatchingQuestionHandler)
-	authRouter.HandleFunc("/matchings", h.MatchingHandler)
-	authRouter.HandleFunc("/matchings/{matchingId}", h.MatchingUserHandler)
-	authRouter.HandleFunc("/messages", h.MessageHandler).Methods("POST")
-	authRouter.HandleFunc("/messages/{messageId}", h.MessageHandler)
-	authRouter.HandleFunc("/question_cards", h.QuestionCardHandler)
+	authRouter.HandleFunc("/matching-questions", matchingQuestionController.CreateMatching).Methods("POST")
+	authRouter.HandleFunc("/matching-questions", matchingQuestionController.GetMatchingQuestion).Methods("GET")
+
+	authRouter.HandleFunc("/question-cards/{matchingId}", questionCardController.GetQuestionCards).Methods("GET")
+
+	authRouter.HandleFunc("/matchings", matchingController.GetMatchings).Methods("GET")
+	authRouter.HandleFunc("/matchings/{matchingId}", matchingController.GetMatchingUser).Methods("GET")
+
+	authRouter.HandleFunc("/messages/{matchingId}", messageController.CreateMessage).Methods("POST")
+	authRouter.HandleFunc("/messages/{matchingId}", messageController.GetMessages).Methods("GET")
+	authRouter.HandleFunc("/messages/{messageId}", messageController.UpdateMessage).Methods("PUT")
 
 	return middleware.CORS(router)
 }
