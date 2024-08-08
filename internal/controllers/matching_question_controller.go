63dc34045bc2a9fa0ccf6cf9a2a76e6158ecfd85
Toshiki Takakubo <219tosiki@gmail.com>
2024-08-08 13:36:47 +0900

    refactor: Add lazy loading for images in matching question controller

--- internal/controllers/matching_question_controller.go
+++ internal/controllers/matching_question_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/coworker-match-api/internal/common"
 	"github.com/coworker-match-api/internal/usecases"
 )
 
@@ -20,9 +21,13 @@ func NewMatchingQuestionController(mqu usecases.IMatchingQuestionUsecase) IMatch
 }
 
 func (mqc *matchingQuestionController) CreateMatching(w http.ResponseWriter, r *http.Request) {
-
 }
 
 func (mqc *matchingQuestionController) GetMatchingQuestion(w http.ResponseWriter, r *http.Request) {
-
+	response, err := mqc.mqu.GetMatchingQuestion()
+	if err != nil {
+		common.RespondWithError(w, http.StatusInternalServerError, "Failed to get matching question")
+		return
+	}
+	common.RespondWithJSON(w, http.StatusOK, response)
 }
--- internal/repositories/matching_question_repo.go
+++ internal/repositories/matching_question_repo.go
@@ -6,6 +6,22 @@ import (
 	models "github.com/coworker-match-api/gen/go"
 )
 
+var choice1Images = []string{
+	"/yes1_image.png",
+	"/yes2_image.png",
+	"/yes3_image.png",
+	"/yes4_image.png",
+	"/yes5_image.png",
+}
+
+var choice2Images = []string{
+	"/no1_image.png",
+	"/no2_image.png",
+	"/no3_image.png",
+	"/no4_image.png",
+	"/no5_image.png",
+}
+
 type IMatchingQuestionRepo interface {
 	CreateMatching(userId string, req models.CreateQuestionReq) (*models.CreateQuestionRes, error)
 	GetMatchingQuestion() (*models.GetQuestionRes, error)
@@ -24,5 +40,38 @@ func (mqr *matchingQuestionRepo) CreateMatching(userId string, req models.Create
 }
 
 func (mqr *matchingQuestionRepo) GetMatchingQuestion() (*models.GetQuestionRes, error) {
-	return nil, nil
+	query := `
+			SELECT
+				question_id,
+				question_text
+			FROM
+				matching_questions
+			LIMIT
+				5;
+			`
+	rows, err := mqr.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var response models.GetQuestionRes
+
+	for rows.Next() {
+		var data models.Question
+		if err := rows.Scan(&data.QuestionId, &data.QuestionText); err != nil {
+			return nil, err
+		}
+
+		/* data.Choice1 = *models.NewChoice("", "")
+		data.Choice2 = *models.NewChoice("", "") */
+
+		data.Choice1.SetChoiceText("YES")
+		data.Choice1.SetChoiceImageUrl(choice1Images[len(response.Questions)%len(choice1Images)])
+		data.Choice2.SetChoiceText("NO")
+		data.Choice2.SetChoiceImageUrl(choice2Images[len(response.Questions)%len(choice2Images)])
+
+		response.Questions = append(response.Questions, data)
+	}
+	return &response, nil
 }
