f271511cc11693441e0f92af7badbcf953ccf41e
Toshiki Takakubo <219tosiki@gmail.com>
2024-09-02 20:18:31 +0900

    refactor: Update GetMatchings method to improve performance and readability

--- internal/repositories/matching_repo.go
+++ internal/repositories/matching_repo.go
@@ -43,42 +43,44 @@ func (mr *matchingRepo) GetMatchingUserId(userId, matchingId string) (string, er
 
 func (mr *matchingRepo) GetMatchings(userId string) (*models.GetMatchingRes, error) {
 	query := `
+		WITH matched_users AS (
 			SELECT
 				m.matching_id,
 				CASE
-					WHEN m.sender_user_id = $1 THEN ru.avatar_url
-					ELSE su.avatar_url
-				END AS avatar_url,
-				CASE
-					WHEN m.sender_user_id = $1 THEN ru.user_name
-					ELSE su.user_name
-				END AS match_user_name,
-				COALESCE(qc.question_card_text, 'メッセージはありません') AS last_message,
-				0 AS unread_message_count
+					WHEN m.sender_user_id = $1 THEN m.receiver_user_id
+					ELSE m.sender_user_id
+				END AS match_user_id,
+				m.created_at AS matching_created_at
 			FROM
 				matchings m
-				JOIN users su ON m.sender_user_id = su.user_id
-				JOIN users ru ON m.receiver_user_id = ru.user_id
-				LEFT JOIN (
-					SELECT DISTINCT ON (matching_id)
-						msg.matching_id,
-						msg.question_card_id,
-						msg.created_at
-					FROM 
-						messages msg
-					JOIN (
-						SELECT
-							matching_id,
-							MAX(created_at) AS max_created_at
-						FROM
-							messages
-						GROUP BY
-							matching_id
-					) max_msgs ON msg.matching_id = max_msgs.matching_id AND msg.created_at = max_msgs.max_created_at
-				) lm ON lm.matching_id = m.matching_id
-				LEFT JOIN question_cards qc ON lm.question_card_id = qc.question_card_id
-			WHERE 
-				su.user_id = $1 OR ru.user_id = $1
+			WHERE
+				m.sender_user_id = $1 OR m.receiver_user_id = $1
+		),
+
+		latest_messages AS (
+			SELECT DISTINCT ON (matching_id)
+				matching_id,
+				question_card_id,
+				updated_at
+			FROM
+				messages
+			ORDER BY
+				matching_id, updated_at DESC
+		)
+
+		SELECT
+			mu.matching_id,
+			u.avatar_url,
+			u.user_name AS match_user_name,
+			COALESCE(qc.question_card_text, 'メッセージはありません') AS last_message,
+			0 AS unread_message_count
+		FROM
+			matched_users mu
+		JOIN users u ON u.user_id = mu.match_user_id
+		LEFT JOIN latest_messages lm ON lm.matching_id = mu.matching_id
+		LEFT JOIN question_cards qc ON lm.question_card_id = qc.question_card_id
+		ORDER BY
+			COALESCE(lm.updated_at, mu.matching_created_at) DESC;
 			`
 	rows, err := mr.db.Query(query, userId)
 	if err != nil {
